d66bd786ce2948a64a517d25d83999d08fd2110e
Zoe <zzoecode@gmail.com>
2021-03-03 12:10:50 +0800

    Modify UI

--- app/src/main/java/com/kairan/esc_project/mappingModeDisplay/DisplayMap.java
+++ app/src/main/java/com/kairan/esc_project/mappingModeDisplay/DisplayMap.java
@@ -1,15 +1,17 @@
-package com.kairan.esc_project;
+package com.kairan.esc_project.mappingModeDisplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TestingMode extends AppCompatActivity {
+import com.kairan.esc_project.R;
+
+public class DisplayMap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_testing_mode);
+        setContentView(R.layout.activity_display_map);
 
     }
 }

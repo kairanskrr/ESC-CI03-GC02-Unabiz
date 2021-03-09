d8f5bab98a2bb357e94774c812d08cd10d1411f3
kairanskrr <kairan_sun@mymail.sutd.edu.sg>
2021-03-10 00:18:47 +0800

    Get (x,y) coordinates
    Able to get raw (x,y) position of the image when performing long press, can check in Logcat using tag "MAPPOSITION"

--- app/src/main/java/com/kairan/esc_project/TestingMode.java
+++ app/src/main/java/com/kairan/esc_project/TestingMode.java
@@ -1,14 +1,48 @@
 package com.kairan.esc_project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class TestingMode extends AppCompatActivity {
+    SubsamplingScaleImageView image_mappedMap;
+    Button button_selectMap;
+    Button button_getLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing_mode);
+        image_mappedMap = findViewById(R.id.image_mappedFloorPlan);
+        button_selectMap = findViewById(R.id.button_selectMap);
+        button_getLocation = findViewById(R.id.button_getLocation);
+
+        button_getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
